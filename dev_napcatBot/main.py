6024b621f9e0508fb1368e8c0a82773e40116dda
梦归云帆 <1138663075@qq.com>
2025-09-28 19:50:15 +0800

    refactor: 更新 ncatbot 版本并优化 bot 启动配置

--- dev_napcatBot/main.py
+++ dev_napcatBot/main.py
@@ -1,13 +1,8 @@
 from ncatbot.core import BotClient
-from ncatbot.utils import config
-from ncatbot.utils import get_log
+from ncatbot.utils import get_log, config
 
 logger = get_log()
-
-config.set_bot_uin("2557441898")  # 设置 bot qq 号 (必填)
-config.set_root("1138663075")  # 设置 bot 超级管理员账号 (建议填写)
-
 bot = BotClient()
 
 if __name__ == "__main__":
-    bot.run_frontend()
+    bot.run_frontend(bt_uin=2557441898, enable_webui_interaction=True)
